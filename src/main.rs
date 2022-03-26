8f7ec90426257339f23f0c1d612a9e71768f55dc
Alaeddine <alaeddinefattouch@insat.u-carthage.tn>
2022-03-26 17:21:30 +0100

    testing the hash trait

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use blockchainlib::*;
 fn main() {
-    let block = Block::new(0,now(), vec![0;32], 0, "Genesis block".to_owned());
-    println!("{:?}", &block);
+    let mut block = Block::new(0,now(), vec![0;32], 0, "Genesis block".to_owned());
 
     let h = block.hash();
+
     println!("{:?}", &h);
+
+    block.hash = h;
+
+    println!("{:?}", &block);
+    
 }
