cb0699a250fd6527e853653b67037fac2e85abf9
Alaeddine <alaeddinefattouch@insat.u-carthage.tn>
2022-03-26 17:14:19 +0100

    added a trait for calculating the hash of a block

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,7 @@ use blockchainlib::*;
 fn main() {
     let block = Block::new(0,now(), vec![0;32], 0, "Genesis block".to_owned());
     println!("{:?}", &block);
+
+    let h = block.hash();
+    println!("{:?}", &h);
 }
